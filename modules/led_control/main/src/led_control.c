d6878e187068db1bb602fce7b35f7e76a1ac8b67
Vladislav Ermachenkov <ermachenkov.v.s@gmail.com>
2020-10-25 22:28:37 +0300

    Magic numbers were moved to 'define' section

--- modules/led_control/main/src/led_control.c
+++ modules/led_control/main/src/led_control.c
@@ -4,6 +4,19 @@
 #include "pwm_timer.h"
 #include "counter_timer.h"
 
+#define kMIN_PCT (0)
+#define kMAX_PCT (100)
+#define kPCT_MIN_STEP (1)
+#define kDMA_BUF_SIZE ((kMAX_PCT * kPCT_MIN_STEP) * 2)
+#define kTIMER_EDGES_PRESCALER (479)
+#define kTIMER_STABLE_PRESCALER (47999)
+
+typedef enum
+{
+  kLED_CONTROL_RC_OK,
+  kLED_CONTROL_RC_ERR,
+}led_control_rc_t;
+
 void led_control_start(led_control_t led_num)
 {
   switch (led_num)
@@ -37,11 +50,11 @@ void led_control_process(void)
 
 }
 
-static int led_control_prepare_data(char *buf, size_t buf_size, size_t max_points)
+static led_control_rc_t led_control_prepare_data(char *buf, size_t buf_size, size_t max_points)
 {
   if ((NULL == buf) || (buf_size < (max_points * 2)))
   {
-    return -1;
+    return kLED_CONTROL_RC_ERR;
   }
 
   for (size_t i = 0; i < max_points; i++)
@@ -50,45 +63,45 @@ static int led_control_prepare_data(char *buf, size_t buf_size, size_t max_point
     buf[((max_points)*2 - 1) - i] = i;
   }
 
-  return 0;
+  return kLED_CONTROL_RC_OK;
 }
 
-static int led_control_calc_preset_data(led_cfg_t *led_cfg, led_desc_t *led_desc)
+static led_control_rc_t led_control_calc_preset_data(led_cfg_t *led_cfg, led_desc_t *led_desc)
 {
   if ((NULL == led_cfg) || (NULL == led_desc))
   {
-    return -1;
+    return kLED_CONTROL_RC_ERR;
   }
 
-  if ((led_cfg->max_brightness_pct > 100) || (0 == led_cfg->max_brightness_pct))
+  if ((led_cfg->max_brightness_pct > kMAX_PCT) || (kMIN_PCT == led_cfg->max_brightness_pct))
   {
-    return -1;
+    return kLED_CONTROL_RC_ERR;
   }
 
   if ((0 == led_cfg->t1_ms) || (0 == led_cfg->t2_ms) || (0 == led_cfg->t3_ms))
   {
-    return -1;
+    return kLED_CONTROL_RC_ERR;
   }
 
-  int rc = prepare_data(led_cfg->data_buf, led_cfg->data_buf_size, led_cfg->max_brightness_pct);
+  led_control_rc_t rc = prepare_data(led_cfg->data_buf, led_cfg->data_buf_size, led_cfg->max_brightness_pct);
 
-  if (0 != rc)
+  if (kLED_CONTROL_RC_OK != rc)
   {
-    return -1;
+    return rc;
   }
 
-  led_desc->rising.tmr_desc.tmr_psc = 479;
-  led_desc->rising.tmr_desc.tmr_arr = (led_cfg->t1_ms * 100) / led_cfg->max_brightness_pct;
+  led_desc->rising.tmr_desc.tmr_psc = kTIMER_EDGES_PRESCALER;
+  led_desc->rising.tmr_desc.tmr_arr = (led_cfg->t1_ms * kMAX_PCT) / led_cfg->max_brightness_pct;
   led_desc->rising.dma_buf_size = led_cfg->max_brightness_pct;
   led_desc->rising.dma_buf = led_cfg->data_buf;
 
-  led_desc->falling.tmr_desc.tmr_psc = 479;
-  led_desc->falling.tmr_desc.tmr_arr = (led_cfg->t2_ms * 100) / led_cfg->max_brightness_pct;
+  led_desc->falling.tmr_desc.tmr_psc = kTIMER_EDGES_PRESCALER;
+  led_desc->falling.tmr_desc.tmr_arr = (led_cfg->t2_ms * kMAX_PCT) / led_cfg->max_brightness_pct;
   led_desc->falling.dma_buf_size = led_cfg->max_brightness_pct;
   led_desc->falling.dma_buf = led_cfg->data_buf + led_cfg->max_brightness_pct;
 
-  led_desc->stable.tmr_psc = 47999;
+  led_desc->stable.tmr_psc = kTIMER_STABLE_PRESCALER;
   led_desc->stable.tmr_arr = led_cfg->t3_ms;
 
-  return 0;
+  return kLED_CONTROL_RC_OK;
 }
