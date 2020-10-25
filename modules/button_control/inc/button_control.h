6d79e4fbe8dd0b73581ec8927a8affe26f093661
Vladislav Ermachenkov <ermachenkov.v.s@gmail.com>
2020-10-25 23:13:28 +0300

    Updated 'button_control' module

---
+++ modules/button_control/inc/button_control.h
@@ -0,0 +1,16 @@
+#ifndef BUTTON_CONTROL_H
+#define BUTTON_CONTROL_H
+
+/**
+ * @brief
+ * @addtogroup
+ * @{
+ */
+
+void button_control_init(void);
+void button_control_process(void);
+void button_control_deinit(void);
+
+#endif // BUTTON_CONTROL_H
+
+/** @} */
---
+++ modules/button_control/src/button_control.c
@@ -0,0 +1,16 @@
+#include "button_control.h"
+
+void button_control_init(void)
+{
+
+}
+
+void button_control_process(void)
+{
+
+}
+
+void button_control_deinit(void)
+{
+
+}
